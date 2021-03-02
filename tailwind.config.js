5c0d977afa1ab9dde5a2672832a60715f7fc5a2b
George Brown <teawares@gmail.com>
2021-03-02 22:08:56 +0800

    ✨ spin

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,23 +2,7 @@ module.exports = {
   purge: ["./pages/**/*.js", "./components/**/*.js", "./layouts/**/*.js"],
   darkMode: "class",
   theme: {
-    extend: {
-      keyframes: {
-        "come-in-out": {
-          "0%": { transform: "scale(0)" },
-          "50% ": { transform: "scale(1)" },
-          "100%": { transform: "scale(0)" },
-        },
-        spin: {
-          "0%": { transform: "rotate(0deg)" },
-          "100%": { transform: "rotate(180deg)" },
-        },
-        animation: {
-          "come-in-out": "'come-in-out' 300ms forwards",
-          spin: "spin 500ms linear forwards",
-        },
-      },
-    },
+    extend: {},
   },
   variants: {},
   plugins: [],
